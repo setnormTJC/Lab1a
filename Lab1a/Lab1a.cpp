0db27df78805fc7b4224f464fc7722fc593814ec
Work <Work@MSI>
2024-01-23 10:01:31 -0600

    Add project files.

---
+++ Lab1a/Lab1a.cpp
@@ -0,0 +1,21 @@
+//; kjalksdjflksadjfl;ksadjf
+//sdf
+//ad
+//solve the world's problems!
+
+#include<iostream> //this "header file" allows the use of cout 
+					//(put Characters OUT - to the terminal)
+
+using namespace std; //standard 
+
+int main()
+{
+	//new "paragraph" 
+	cout << "Hello, world?" << endl; 
+
+
+
+
+
+}
+
